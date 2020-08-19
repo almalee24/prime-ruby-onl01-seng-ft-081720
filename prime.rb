d47022374a6380c400aa3c807fe5509fcd62250a
Alma Flores <flores.almalee@gmail.com>
2020-08-19 02:43:00 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,12 @@
 # Add  code here!
 def prime?(num)
-  if num < 0
-    false 
-  end
   n = 2 
   while n < num
     false if num % n == 0 
     n += 1 
   end
   true
+  if num < 0
+    false 
+  end
 end
